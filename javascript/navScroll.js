a72318cbddf49155da963a4735e115c3723e567f
Your Name <you@example.com>
2024-11-18 17:00:18 +0000

    shows page

--- javascript/navScroll.js
+++ javascript/navScroll.js
@@ -1,6 +1,10 @@
+// this script changed the background colour of teh header between black and transparent
+// depending on the navs position on screen
+
 const nav = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
+  // specifiy the beginning and end 
   const scrollPosition =
     document.documentElement.scrollTop || document.body.scrollTop;
 
